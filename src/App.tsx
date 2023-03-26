24ebad7df3a275a15c6d40c42e81cb8dba7b1108
Antonio Victor <Antoniovictor12@live.com>
2023-03-26 12:50:07 -0300

    feat(App): adicionar setup context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import { AuthContextProvider } from './contexts/AuthContext';
 import Router from './routes/router';
 
 const queryClient = new QueryClient();
@@ -7,7 +8,9 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <AuthContextProvider>
+        <Router />
+      </AuthContextProvider>
     </QueryClientProvider>
   );
 }
