7ce8c424ebd77bc9220d0aa338180467c6785910
Antonio Victor <Antoniovictor12@live.com>
2023-03-29 18:40:20 -0300

    feat(FormControl): adicionar render prop

--- src/components/forms/FormControl/index.tsx
+++ src/components/forms/FormControl/index.tsx
@@ -1,37 +1,33 @@
-import { useForm, FieldValues, Path, DeepPartial } from 'react-hook-form';
+import { ReactNode } from 'react';
+import {
+  useForm,
+  FieldValues,
+  DeepPartial,
+  Control,
+  FieldErrors,
+} from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ZodSchema } from 'zod';
 import { SxProps, Theme } from '@mui/material';
 
 import Box from '@mui/material/Box';
 
-import InputText from '~/components/forms/InputText';
-
-export interface FormControlInput<T> {
-  name: Path<T>;
-  label: string;
-}
-
 interface FormControlProps<T extends FieldValues> {
-  inputs: FormControlInput<T>[];
-  inputMargin: 'none' | 'normal' | 'dense';
-  inputVariant: 'outlined' | 'filled' | 'standard';
   formValidationSchema: ZodSchema<T>;
   formId: string;
   defaultValues: DeepPartial<T> | null;
   styles: SxProps<Theme>;
   onSubmit: (data: T) => void;
+  render: (control: Control<T, unknown>, errors: FieldErrors<T>) => ReactNode;
 }
 
 function FormControl<T extends FieldValues>({
-  inputs,
-  inputMargin,
-  inputVariant,
   formValidationSchema,
   formId,
   defaultValues,
   styles,
   onSubmit,
+  render,
 }: FormControlProps<T>) {
   const {
     control,
@@ -50,18 +46,7 @@ function FormControl<T extends FieldValues>({
       onSubmit={handleSubmit(onSubmit)}
       sx={styles}
     >
-      {inputs.map(({ name, label }) => (
-        <InputText
-          key={name}
-          name={name}
-          label={label}
-          validation={control}
-          invalid={!!errors[name]}
-          helperText={errors[name]?.message as string}
-          margin={inputMargin}
-          variant={inputVariant}
-        />
-      ))}
+      {render(control, errors)}
     </Box>
   );
 }
