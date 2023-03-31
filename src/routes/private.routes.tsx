6fb31ad8e68ac39f934f86780773ce3ea4ed39c9
Antonio Victor <Antoniovictor12@live.com>
2023-03-31 07:22:59 -0300

    feat(routes): adicionar rota vehicle-supply-panel

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import DashBoard from '~/pages/DashBoard';
 import AsideMenu from '~/pages/DashBoard/components/AsideMenu';
 import VehicleRegistrationPanel from '~/pages/DashBoard/components/VehicleRegistrationPanel';
+import VehicleSupplyRegistrationPanel from '~/pages/DashBoard/components/VehicleSupplyRegistrationPanel';
 
 function PublicRoutes() {
   return (
@@ -12,6 +13,10 @@ function PublicRoutes() {
         element={<DashBoard asideMenu={<AsideMenu />} />}
       >
         <Route path="vehicle-panel" element={<VehicleRegistrationPanel />} />
+        <Route
+          path="vehicle-supply-panel"
+          element={<VehicleSupplyRegistrationPanel />}
+        />
       </Route>
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
